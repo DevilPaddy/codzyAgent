cc0eae76d0e8bafe9753d7d94d0e555a1eb3b6c4
Devilpaddy <belsareanuj7+git@gmail.com>
2025-11-27 23:48:06 +0530

    added the middleware

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -22,7 +22,7 @@ export default function () {
                         <p className="text-black md:text-[3em] text-center text-[1.4em] p-2 font-bold leading-tight">It's <span className="text-red-600 underline">404</span> ERROR</p>
                     </div>
                 </div>
-                <div className="w-screen text-center">
+                <div className="w-screen mt-[-10em] md:mt-[-6em] text-center">
                     <Link
                         href={'/'}
                         className="text-blue-700 text-lg underline"
