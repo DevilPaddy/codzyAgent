8658cc3c1486924f7fa04e88abd25ee3ce5ab9fc
Devilpaddy <belsareanuj7+git@gmail.com>
2025-11-28 23:35:12 +0530

    added some footer content

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -6,7 +6,7 @@ import { LuMaximize2, LuLoader } from "react-icons/lu";
 import { TbArrowsDiagonalMinimize } from "react-icons/tb";
 import { FiDownload, FiSend, FiSave } from "react-icons/fi"; 
 
-const API_BASE_URL = process.env.AI_URL;
+const API_BASE_URL = "http://54.162.30.59:5000";
 
 const PreviewContent = () => {
     const searchParams = useSearchParams();
